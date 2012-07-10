ea369a0cb8a0a2ec3024994907ccf9cd1ca54d8f
Fergal Condron <condrof@gmail.com>
2012-07-10 01:05:11 +0100

    fixing some issues

--- db/migrate/20120709162930_create_teams.rb
+++ db/migrate/20120709162930_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration
     create_table :teams do |t|
       t.string :teamname
       t.integer :points
+      t.integer :user_id
 
       t.timestamps
     end
