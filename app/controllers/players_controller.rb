726db4a8409b3b0c28084dacf4fd75a05b8bcd47
Fergal Condron <condrof@gmail.com>
2012-08-05 16:45:27 +0100

    fixed some errors

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,7 @@
 class PlayersController < ApplicationController
   load_and_authorize_resource
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
   def index
     @q = Player.search(params[:q])
     @players = @q.result(:distinct => true)  #.page(params[:page]).per(40)
@@ -8,6 +10,13 @@ class PlayersController < ApplicationController
   end
 
   def show
+
   end
+  
+protected
+
+  def record_not_found
+    redirect_to players_path(params)
+  end 
 
 end
